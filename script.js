55b0ca34010c649859a58d81d4a2202c1d5bcb55
Jeeva2709 <73710488+Jeeva2709@users.noreply.github.com>
2020-10-30 21:44:33 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -34,4 +34,12 @@ window.onload = () => {
         beat: "./assets/Musical Compos 33.mp3",
         color:  "#db61d5"
       },
+      9: {
+        beat: "./assets/Musical Orches 4.mp3",
+        color: " #db61d5"
+      }     
+}
+const setButtons = () => {
+    for(let beatKey in beats){
+    }
 }
