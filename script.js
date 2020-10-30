34f921442946fc77694ce45728f054d344c723f9
RishiRio143 <73685339+RishiRio143@users.noreply.github.com>
2020-10-30 22:32:59 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -52,5 +52,6 @@ window.onload = () => {
 
     const onButtonPress = (buttonKey) => {
         let element = document.getElementById(buttonKey);
+        element.style.backgroundColor = beats[buttonKey].color;
     }
   }
