28546ccf6ef6c4e60ee8921139c7a38f9c471f13
Sukirtharaj05 <73687232+Sukirtharaj05@users.noreply.github.com>
2020-10-30 21:00:59 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -6,4 +6,8 @@ window.onload = () => {
       beat: "./assets/Piano Chord 331.mp3",
       color: " #db61d5"
     },
+    2: {
+        beat: "./assets/Piano Chord 209.mp3",
+        color: " #db61d5"
+      },
 }
