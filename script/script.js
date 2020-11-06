83af2f9b4105f17efa06aa1d7e41a2357b6aba12
karthibabu <54367776+karthibabu10@users.noreply.github.com>
2020-11-06 13:11:36 +0530

    updated
    updated

---
+++ script/script.js
@@ -0,0 +1,79 @@
+
+
+
+
+
+window.onload = () => {
+  setButtons();
+}
+
+let beats = {
+  1: {
+    beat: "./audio/Piano Chord 331.mp3",
+    color: " #db61d5"
+  },
+  2: {
+    beat: "./audio/Piano Chord 209.mp3",
+    color: " #db61d5"
+  },
+  3: {
+    beat: "./audio/Piano Chord 208.mp3",
+    color: " #db61d5"
+  },
+  4: {
+    beat: "./audio/Drum Sticks Hit 3.mp3",
+    color:  "#db61d5"
+  },
+  5: {
+    beat: "./audio/Drum Snare Roll.mp3",
+    color: " #db61d5"
+  },
+  6: {
+    beat: "./audio/PREL Musical 57.mp3",
+    color: " #db61d5"
+  },
+  7: {
+    beat: "./assets/Cymbal Suspended 2.mp3",
+    color: " #db61d5"
+  },
+  8: {
+    beat: "./audio/Musical Compos 33.mp3",
+    color:  "#db61d5"
+  },
+  9: {
+    beat: "./audio/Musical Orches 4.mp3",
+    color: " #db61d5"
+  }
+}
+
+
+const setButtons = () => {
+  for(let beatKey in beats){
+    let beat = beats[beatKey];
+    let element = document.getElementById(beatKey);
+    element.style.borderColor = beat.color;
+    element.addEventListener("transitionend",  ()=>{
+      element.style.backgroundColor = "transparent";
+      element.style.boxShadow = "none";
+    })
+  }
+}
+
+const onButtonPress = (buttonKey) => {
+  let element = document.getElementById(buttonKey);
+  element.style.backgroundColor = beats[buttonKey].color;
+  element.style.boxShadow = `0px 0px 12px 0px ${beats[buttonKey].color}`;
+}
+
+
+const playBeat = (buttonKey) => {
+  let audioSrc = beats[buttonKey].beat;
+  let audio = new Audio(audioSrc);
+  audio.currentTime = 0;
+  audio.play();
+}
+
+document.addEventListener('keydown', (event)=>{
+  playBeat(event.key);
+  onButtonPress(event.key);
+})
